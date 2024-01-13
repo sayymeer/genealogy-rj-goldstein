61c056fa4cc1d524834b09daf5e5c483d7562d0f
Naman Kumar Jaiswal <namankkumarjjaiswal@gmail.com>
2024-01-13 22:08:01 +0530

    Cleared the Clutter around Carl Von Linde

--- script.js
+++ script.js
@@ -14,40 +14,6 @@ const linewidth = 1.5
 const rectradius = 8
 
 function createRectWithWrappedText(svg, x, y, text,body,flag) {
-  if(flag == 1){
-
-    const foreignObject = svg.append("foreignObject")
-    .attr("x", x)
-    .attr("y", y)
-    .attr("width", rectwidth)
-    .attr("height", rectheight*2.5).attr("class",text)
-    svg.append("rect")
-    .attr("x", x).attr("rx",rectradius)
-    .attr("y", y).attr("ry",rectradius)
-    .classed(text,true)
-    .attr("width", rectwidth)
-    .attr("height", rectheight*2.5)
-    .attr("fill", "none")
-    .attr("stroke", "maroon")
-    .attr("stroke-width", 3.5);
-
-  // Create a foreignObject to embed HTML content (for wrapped text)
-
-  // Add a div inside foreignObject for wrapped text
-  const div = foreignObject.append("xhtml:section")
-    .style("width", rectwidth)
-    .style("height", rectheight*2.5)
-    .style("overflow", "hidden")
-    .style("text-align", "center")
-    .style("font-size", "16px")
-    .style("padding","2px")
-
-
-  // Add the wrapped text
-  div.html(`<h4 style="margin-bottom:0px; color: #800000;font-weight: bolder; font-size: 16px">Carl Von Linde</h4><p style="margin-top:0px;font-style: italic;color: #800000; font-size: 14px">(1902) Prof. Applied Thermodynamics </p> <hr><h4 style="margin-bottom:0px; color: #800000;font-weight: bolder; font-size: 16px">Oscar Knoblauch</h4><p style="margin-top:0px;font-style: italic;color: #800000; font-size: 14px">Inst. Tech. Physics (1902) <br> - W Nusselt (1907)<br> - Ernst Schmidt (1925)</p>`);
-  return
-  }
-
   const foreignObject = svg.append("foreignObject")
     .attr("x", x)
     .attr("y", y)
